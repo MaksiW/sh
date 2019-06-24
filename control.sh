20b518e620cb4270c3f746f6d60fde9cdf2b0dd3
MaksiW <45657787+MaksiW@users.noreply.github.com>
2019-06-24 22:35:10 +0300

    Update control.sh

--- control.sh
+++ control.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
  
-cd /home/opt/BitShark/bch
+cd /home/opt/bch
 # Check run proccess
 if ! pgrep -f "Example.py" > /dev/null
 then
     echo "[!] Start new proccess"
-    cd /home/opt/BitShark/bch/
+    cd /home/opt/bch/
     python ./Example.py > /dev/null &
 fi
